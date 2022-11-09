5d7406e3271160855d263b419aa5a8d85c5090ca
Bean <98428255+bean-mhm@users.noreply.github.com>
2022-11-09 10:11:34 +0000

    new CMF class (empty) for loading color matching function tables from csv files added rapidcsv library for parsing csvs

---
+++ RealBloomCPP/src/ColorManagement/CMF.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct CmfTable
+{
+    float start;
+    float end;
+    float step;
+    size_t count;
+};
+
+class CMF
+{};
+
