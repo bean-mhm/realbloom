b4a3e5c9e0aac2999ba3a2ed2b7716c9b5619d46
Bean <98428255+bean-mhm@users.noreply.github.com>
2023-01-29 11:15:19 +0000

    updated anim_conv.py to work with the new interactive CLI

--- RealBloom/demo/Scripts/anim_conv.py
+++ RealBloom/demo/Scripts/anim_conv.py
@@ -5,38 +5,43 @@ import subprocess
 import time
 
 # Paths
-rb_path = "path to RealBloom.exe"
-frames_dir = "path to the folder containing the frames, must end with a (back)slash"
-kernel_path = "path to the kernel"
-output_dir = "path to the folder containing the output frames, must end with a (back)slash"
+rb_path = 'path to RealBloom.exe'
+frames_dir = 'path to the folder containing the frames, must end with a (back)slash'
+kernel_path = 'path to the kernel'
+output_dir = 'path to the folder containing the output frames, must end with a (back)slash'
 
 # Frames
 frame_start = 1
 frame_end = 50
 
 # Example: frame0001.exr
-frame_prefix = "frame"
+frame_prefix = 'frame'
 frame_length = 4
-frame_suffix = ".exr"
+frame_suffix = '.exr'
 
-# Color Spaces (w = working space)
-frames_cs = "w"
-kernel_cs = "w"
-output_cs = "w"
+# Color spaces (w = working space)
+frames_cs = 'w'
+kernel_cs = 'w'
+output_cs = 'w'
 
-# Additional Arguments
-arguments = "--kernel-contrast 0.05 --kernel-crop 0.8 --autoexp --mix 0.15"
+# Additional arguments
+arguments = '--kernel-contrast 0.05 --kernel-crop 0.8 --autoexp --mix 0.15'
 
-"""
+'''
 Arguments:
   --input, -i             * Input filename
   --input-space, -a       * Input color space
   --kernel, -k            * Kernel filename
   --kernel-space, -b      * Kernel color space
   --output, -o            * Output filename
-  --output-space, -c      * Output color space
+  --output-space, -p      Output color space (no view transform)
+  --display, -h           Display name for view transform
+  --view, -j              View name for view transform
+  --look, -l              Look name for view transform
+  --view-exp              Exposure for view transform [def: 0]
   --kernel-exposure, -e   Kernel exposure [def: 0]
-  --kernel-contrast, -f   Kernel contrast [def: 0]
+  --kernel-contrast, -c   Kernel contrast [def: 0]
+  --kernel-color, -f      Kernel color [def: 1,1,1]
   --kernel-rotation, -r   Kernel rotation [def: 0]
   --kernel-scale, -s      Kernel scale [def: 1]
   --kernel-crop, -q       Kernel crop [def: 1]
@@ -49,22 +54,72 @@ Arguments:
   --mix, -m               Blend between input and output (normal blending) [def: 1]
   --conv-exposure, -z     Conv. output exposure [def: 0]
   --verbose, -v           Enable verbose logging
-"""
+'''
 
+# Print the command line output from RealBloom
+def print_stdout(proc: subprocess.Popen):
+    s = ''
+    last_char = '\n'
+    
+    while True:
+        char = proc.stdout.read(1).decode()
+        
+        if len(char) < 1:
+            break
+        
+        if last_char == '\n' and char == '>':
+            break
+        
+        last_char = char
+        
+        s += char
+    
+    s = s.strip()
+    if (s != ''):
+        # Left padding
+        s = '  ' + s
+        s = s.replace('\n', '\n  ')
+        
+        # Vertical padding
+        if '\n' in s:
+            print(f'\n{s}\n')
+        else:
+            print(s)
+
+# Create subprocess
+proc = subprocess.Popen(
+    [rb_path, 'cli'],
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE)
+print_stdout(proc)
+
+# Process the frames
 for i in range(frame_start, frame_end + 1):
     filename = frame_prefix + str(i).zfill(frame_length) + frame_suffix
-    command = "\"{}\" conv -i \"{}\" -a \"{}\" -k \"{}\" -b \"{}\" -o \"{}\" -c \"{}\" {}"
-    command = command.format(
-        rb_path,
-        frames_dir + filename,
-        frames_cs,
-        kernel_path,
-        kernel_cs,
-        output_dir + filename,
-        output_cs,
-        arguments
-    )
+    
+    command = f'conv -i "{frames_dir + filename}" -a "{frames_cs}" -k "{kernel_path}" -b "{kernel_cs}" '
+    command += f'-o "{output_dir + filename}" -p "{output_cs}" {arguments}'
+    
+    # Run
     start = time.time()
-    subprocess.call(command)
+    proc.stdin.write(f'{command}\n'.encode())
+    proc.stdin.flush()
+    print_stdout(proc)
     end = time.time()
-    print(filename + " done in {:.1f} ms".format((end - start) * 1000.0))
+    
+    print(f'{filename} done in {(end - start) * 1000.0:.1f} ms')
+
+print('')
+
+# Quit
+
+proc.stdin.write('quit\n'.encode())
+proc.stdin.flush()
+print_stdout(proc)
+
+proc.stdin.close()
+proc.terminate()
+proc.wait(timeout=5)
+
+print('All frames done')
