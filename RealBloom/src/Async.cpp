832871d519a1df8b077810c846740b5bb05b7690
Bean <98428255+bean-mhm@users.noreply.github.com>
2023-04-10 16:35:40 +0000

    Async:   - renamed tasks to jobs   - renamed schedule() to scheduleJob()   - added processJobs() for processing the queued jobs   - minor changes

--- RealBloom/src/Async.cpp
+++ RealBloom/src/Async.cpp
@@ -3,8 +3,8 @@
 std::vector<std::pair<std::string, SignalValue>> Async::S_SIGNALS;
 std::mutex Async::S_SIGNALS_MUTEX;
 
-std::deque<std::packaged_task<void()>> Async::S_TASKS;
-std::mutex Async::S_TASKS_MUTEX;
+std::deque<std::packaged_task<void()>> Async::S_JOBS;
+std::mutex Async::S_JOBS_MUTEX;
 
 void Async::emitSignal(const std::string& name, const SignalValue& value)
 {
@@ -38,9 +38,45 @@ bool Async::readSignalLast(const std::string& name, SignalValue& outValue)
     return found;
 }
 
-void Async::schedule(std::function<void()> job)
+void Async::scheduleJob(std::function<void()> job, bool wait)
 {
     std::packaged_task<void()> task(job);
-    std::scoped_lock lock(S_TASKS_MUTEX);
-    S_TASKS.push_back(std::move(task));
+    auto future = task.get_future();
+
+    {
+        std::scoped_lock lock(S_JOBS_MUTEX);
+        S_JOBS.push_back(std::move(task));
+    }
+
+    if (wait)
+    {
+        future.get();
+    }
+}
+
+void Async::processJobs(const std::string& threadName)
+{
+    std::unique_lock<std::mutex> lock(S_JOBS_MUTEX);
+
+    while (!Async::S_JOBS.empty())
+    {
+        auto task(std::move(S_JOBS.front()));
+        S_JOBS.pop_front();
+
+        // Retrieve the associated future
+        auto future = task.get_future();
+
+        // Unlock during the job
+        lock.unlock();
+        try
+        {
+            task();
+            future.get();
+        }
+        catch (const std::exception& e)
+        {
+            printError(strFormat("%s (Thread: \"%s\")", __FUNCTION__, threadName.c_str()), "", e.what());
+        }
+        lock.lock();
+    }
 }
--- RealBloom/src/Async.h
+++ RealBloom/src/Async.h
@@ -10,6 +10,8 @@
 #include <unordered_map>
 #include <variant>
 
+#include "Utils/Misc.h"
+
 typedef std::variant<void*, bool, int32_t, uint32_t, int64_t, uint64_t, float, double, std::string> SignalValue;
 
 class Async
@@ -18,18 +20,19 @@ private:
     static std::vector<std::pair<std::string, SignalValue>> S_SIGNALS;
     static std::mutex S_SIGNALS_MUTEX;
 
+    static std::deque<std::packaged_task<void()>> S_JOBS;
+    static std::mutex S_JOBS_MUTEX;
+
 public:
     Async() = delete;
     Async(const Async&) = delete;
     Async& operator= (const Async&) = delete;
 
-    static std::deque<std::packaged_task<void()>> S_TASKS;
-    static std::mutex S_TASKS_MUTEX;
-
     static void emitSignal(const std::string& name, const SignalValue& value);
     static bool readSignal(const std::string& name, SignalValue& outValue);
     static bool readSignalLast(const std::string& name, SignalValue& outValue);
 
-    static void schedule(std::function<void()> job);
+    static void scheduleJob(std::function<void()> job, bool wait);
+    static void processJobs(const std::string& threadName);
 
 };
